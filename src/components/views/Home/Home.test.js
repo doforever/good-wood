45ffb67a1871658bd9066e5b165ced0a950b8e0d
Dorota Tomala <doforever@gmail.com>
2021-06-10 11:39:27 +0200

    Fix Home test by mocking useDispatch

--- src/components/views/Home/Home.test.js
+++ src/components/views/Home/Home.test.js
@@ -3,8 +3,14 @@ import { shallow } from 'enzyme';
 import Home from './Home';
 
 describe('Component Home', () => {
+  beforeAll(() => {
+    const ReactRedux = jest.requireActual('react-redux');
+    ReactRedux.useDispatch = jest.fn(() => null);
+  });
+
   it('should render without crashing', () => {
     const component = shallow(<Home />);
     expect(component).toBeTruthy();
   });
+
 });
