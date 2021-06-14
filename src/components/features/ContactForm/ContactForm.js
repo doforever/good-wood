b557a54f2a35ae1fa2c0c10d7abe30a3797dbe5b
Dorota Tomala <doforever@gmail.com>
2021-06-14 11:09:09 +0200

    Add form visual validation

--- src/components/features/ContactForm/ContactForm.js
+++ src/components/features/ContactForm/ContactForm.js
@@ -18,7 +18,42 @@ const ContactForm = () =>{
     address: '',
   });
 
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+  });
+
+  const validators = {
+    firstName: {
+      validator: value => value && 3 <= value.length && value.length <= 15,
+      message: 'Invalid length',
+    },
+    lastName: {
+      validator: value => value && 3 <= value.length && value.length <= 30,
+      message: 'Invalid length',
+    },
+    email: {
+      validator: value => value && value.match(new RegExp(/^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]{1,6}))$/i)),
+      message: 'Invalid email',
+    },
+    address: {
+      validator: value => value && 5 <= value.length && value.length <= 60,
+      message: 'Invalid length',
+    },
+  };
+
+  const validate = (name, value) => {
+    const {validator, message} = validators[name];
+
+    if (!validator(value)){
+      setErrors({...errors, [name]: message});
+    } else setErrors({...errors, [name]: ''});
+  };
+
   const handleChange = ({target: {name, value}}) => {
+    validate(name, value);
     setFormData({...formData, [name]: value });
   };
 
@@ -37,6 +72,9 @@ const ContactForm = () =>{
               margin='normal'
               value={formData.firstName}
               onChange={handleChange}
+              error={!!errors.firstName}
+              helperText={errors.firstName}
+              required
             />
             <TextField
               id='lastName'
@@ -47,6 +85,9 @@ const ContactForm = () =>{
               margin='normal'
               value={formData.lastName}
               onChange={handleChange}
+              error={!!errors.lastName}
+              helperText={errors.lastName}
+              required
             />
           </Grid>
           <Grid item container xs={12} sm={6} direction='column' justify='flex-start'>
@@ -59,17 +100,23 @@ const ContactForm = () =>{
               margin='normal'
               value={formData.email}
               onChange={handleChange}
+              error={!!errors.email}
+              helperText={errors.email}
+              required
             />
             <TextField
               id='address'
               name='address'
               label="Delivery address"
-              multiline rows={2}
+              multiline rows={3}
               variant='outlined'
               fullWidth
               margin='normal'
               value={formData.address}
               onChange={handleChange}
+              error={!!errors.address}
+              helperText={errors.address}
+              required
             />
           </Grid>
         </Grid>
