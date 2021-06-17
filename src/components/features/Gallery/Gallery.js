e797638e4bed9b876f323dc8aa8ce340a742c1df
Dorota Tomala <doforever@gmail.com>
2021-06-17 22:22:05 +0200

    Set gallery transitions

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -14,22 +14,37 @@ import styles from './Gallery.module.scss';
 
 const Gallery = ({ pictures }) => {
   const [current, setCurrent] = useState(0);
+  const [incoming, setIncoming] = useState(-1);
+  const [isChanging, setIsChanging] = useState(false);
   const matchesSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
 
+  const changePic = (i) => {
+    setIncoming(i);
+    setIsChanging(true);
+    setTimeout(() => {
+      setCurrent(i);
+      setIsChanging(false);
+    }, 1000);
+  };
+
   const thumbnails = pictures.map(({src, name}, i) => i < 5 && (
-    <Grid key={i} item xs className={clxs(styles.thumbnail, i === current && styles.active)}>
-      <ButtonBase onClick={() => setCurrent(i)}>
+    <Grid
+      key={i}
+      item xs
+      className={clxs(styles.thumbnail, (incoming >= 0 ? i === incoming : i === current) && styles.active)}
+    >
+      <ButtonBase onClick={() => changePic(i)}>
         <img src={src} alt={name}/>
       </ButtonBase>
     </Grid>)
   );
 
   const handleNext = () => {
-    setCurrent(current + 1);
+    changePic(current + 1);
   };
 
   const handleBack = () => {
-    setCurrent(current - 1);
+    changePic(current - 1);
   };
 
   const dots = (<MobileStepper
@@ -56,8 +71,14 @@ const Gallery = ({ pictures }) => {
 
   return (
     <Grid container direction='column' className={styles.root}>
-      <Grid item xs className={styles.currentPhoto}>
-        <img src={pictures[current].src} alt={pictures[current].name} />
+      <Grid item xs className={clxs(styles.photo_wrapper, isChanging && styles.changing)}>
+        {incoming >= 0
+          && <img className={styles.incoming_img} src={pictures[incoming].src} alt={pictures[incoming].name} />}
+        <img
+          className={styles.current_img}
+          src={pictures[current].src}
+          alt={pictures[current].name}
+        />
       </Grid>
       { pictures.length > 0 && <Grid item container direction='row'>
         {controls}
