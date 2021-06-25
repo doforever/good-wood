a335e696bd33acf055e703a003938eb331493e11
Dorota Tomala <doforever@gmail.com>
2021-06-25 16:27:40 +0200

    Prepare Heroku deploy

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,13 @@
-const dbURI = process.env.NODE_ENV === 'test'
-  ? 'mongodb://localhost:27017/goodWoodTest'
-  : 'mongodb://localhost:27017/goodWood062020';
+let dbURI;
+switch (process.env.NODE_ENV) {
+  case 'test':
+    dbURI = 'mongodb://localhost:27017/goodWoodTest';
+    break;
+  case 'production':
+    dbURI = `mongodb+srv://doforever:${process.env.dbpass}@goodwooddb.sa1od.mongodb.net/GoodWoodDB?retryWrites=true&w=majority`;
+    break;
+  default:
+    dbURI = 'mongodb://localhost:27017/goodWood062020';
+}
 
 module.exports = {dbURI};
