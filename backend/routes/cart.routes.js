c2c28295fe2912d997bd86a35b507d8c60348777
Dorota Tomala <doforever@gmail.com>
2021-06-29 10:20:52 +0200

    Add cart endpoints, no tests

---
+++ backend/routes/cart.routes.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const Cart = require('../models/cart.model');
+const sanitize = require('mongo-sanitize');
+
+const router = express.Router();
+
+router.get('/carts/stored', async (req, res) => {
+  const cartId = !!req.session && req.session.cartId;
+
+  if (cartId) {
+    try {
+      const storedCart = await Cart.findById(cartId).populate('products.product');
+      if (!storedCart) res.status(404).json({ message: 'Not found' });
+      else res.json(storedCart);
+    }
+    catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  } else res.status(404).json({ message: 'Not found'});
+});
+
+router.post('/carts', async (req, res) => {
+  const { products } = req.body;
+  const session = req.session;
+
+  if (products && products.length > 0 && session) {
+    try {
+      const newCart = new Cart({ products: sanitize(products) });
+      const saved = await newCart.save();
+      session.cartId = saved._id;
+      res.status(201).json(saved);
+    }
+    catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Cart saving error' });
+    }
+  } else {
+    res.status(400).json({ message: 'Bad request' });
+  }
+});
+
+router.put('/carts/stored', async (req, res) => {
+  const { products } = req.body;
+  const cartId = !!req.session && req.session.cartId;
+
+  if (products && cartId) {
+    try {
+      const storedCart = await Cart.findById(cartId).populate('products.product');
+      if (!storedCart) res.status(404).json({ message: 'Not found' });
+      else {
+        Object.assign(storedCart, {
+          products,
+        });
+        const updatedCart = await storedCart.save();
+        res.json(updatedCart);
+      }
+    }
+    catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Cart update error' });
+    }
+  } else {
+    res.status(400).json({ message: 'Bad request' });
+  }
+});
+
+router.delete('/cart/stored', async (req, res) => {
+  const cartId = !!req.session && req.session.cartId;
+
+  if (cartId) {
+    try {
+      const storedCart = await Cart.findById(cartId);
+      if (storedCart) {
+        await storedCart.remove();
+        req.session.cartId = null;
+        res.json(storedCart);
+      }
+      else res.status(404).json({ message: 'Not found...' });
+    }
+    catch (err) {
+      res.status(500).json({ message: 'Remove cart error' });
+    }
+  } else res.status(404).json({ message: 'Not found' });
+});
+
+module.exports = router;
--- backend/routes/orders.routes.js
+++ backend/routes/orders.routes.js
@@ -20,7 +20,7 @@ router.get('/orders', async (req, res) => {
 
 router.get('/orders/:id', async (req, res) => {
   try {
-    const result = await Order.findById(req.params.id).populate('products.product');
+    const result = await Order.findById(req.params.id).populate('cart');
     if (!result) res.status(404).json({ product: 'Not found' });
     else res.json(result);
   }
@@ -31,25 +31,27 @@ router.get('/orders/:id', async (req, res) => {
 });
 
 router.post('/orders', async (req, res) => {
-  const { firstName, lastName, email, address, products, status} = req.body;
+  const { firstName, lastName, email, address } = req.body;
+  const cartId = !!req.session && req.session.cartId;
 
-  if (firstName && lastName && email && address && products && products.length > 0 && status) {
-    try {
-      const newOrder = new Order({
-        firstName: sanitize(firstName),
-        lastName: sanitize(lastName),
-        email,
-        address: sanitize(address),
-        products: sanitize(products),
-        status,
-      });
-      const saved = await newOrder.save();
-      res.status(201).json(saved);
-    }
-    catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Order saving error' });
-    }
+  if (firstName && lastName && email && address) {
+    if (cartId) {
+      try {
+        const newOrder = new Order({
+          firstName: sanitize(firstName),
+          lastName: sanitize(lastName),
+          email,
+          address: sanitize(address),
+          cart: cartId,
+        });
+        const saved = await newOrder.save();
+        res.status(201).json(saved);
+      }
+      catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Order saving error' });
+      }
+    } else res.status(500).json({message: 'Missing cart data'});
   } else {
     res.status(400).json({ message: 'Bad request' });
   }
