6050380046dac47830d5c234dd6485578c6130d5
Dorota Tomala <doforever@gmail.com>
2021-06-29 16:29:20 +0200

    Show completed stepper after sending order

--- src/components/layout/ShoppingStepper/ShoppingStepper.js
+++ src/components/layout/ShoppingStepper/ShoppingStepper.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { getProducts } from '../../../redux/cartRedux';
+import { getRequest } from '../../../redux/orderRedux';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Stepper from '@material-ui/core/Stepper';
@@ -15,7 +16,9 @@ import styles from './ShoppingStepper.module.scss';
 const ShoppingStepper = ({children}) => {
   let location = useLocation();
   const products = useSelector(getProducts);
+  const orderRequest = useSelector(getRequest);
   const [completed, setCompleted] = useState();
+  const [isSending, setIsSending] = useState(false);
   const matchesSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
 
   const steps = [
@@ -25,11 +28,20 @@ const ShoppingStepper = ({children}) => {
   ];
 
   useEffect(() => {
-    if (products.length > 0 ) {
+    if (orderRequest.active) setIsSending(true);
+    if (orderRequest.error) setIsSending(false);
+  }, [orderRequest]);
+
+  useEffect(() => {
+    if (!isSending && products.length > 0 ) {
       if (location.pathname === '/cart') setCompleted(0);
       else setCompleted(1);
     } else setCompleted(-1);
-  }, [location, products]);
+    if (isSending && !orderRequest.active && !orderRequest.error) {
+      setTimeout(() => setIsSending(false), 10000);
+      setCompleted(2);
+    }
+  }, [location, products, orderRequest, isSending]);
 
   const getPosition = () => {
     if (location.pathname ==='/order') return 2;
