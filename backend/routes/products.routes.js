b9c185b9ae677e49a38ba62780331e7f5be30bad
Dorota Tomala <doforever@gmail.com>
2021-07-02 12:07:56 +0200

    Update cache control to stale-while-revalidate

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -8,17 +8,18 @@ router.get('/products', async (req, res) => {
     await Product.find({}, (err, products) => {
       if(err) res.status(500).json(err);
       if (products) {
-        res.header('Cache-Control', 'max-age=7200').json(products.map(({_id, name, defaultPrice, photos}) => (
-          {
-            _id,
-            name,
-            defaultPrice,
-            mainPhoto: {
-              name: photos[0].name,
-              src: photos[0].src,
-            },
-          }
-        )));
+        res.header('Cache-Control', 'max-age=720, stale-while-revalidate=7200')
+          .json(products.map(({_id, name, defaultPrice, photos}) => (
+            {
+              _id,
+              name,
+              defaultPrice,
+              mainPhoto: {
+                name: photos[0].name,
+                src: photos[0].src,
+              },
+            }
+          )));
       } else res.status(404).json({ product: 'Not found' });
     });
   }
@@ -32,7 +33,7 @@ router.get('/products/:id', async (req, res) => {
   try {
     const result = await Product.findById(req.params.id);
     if (!result) res.status(404).json({ product: 'Not found' });
-    else res.json(result);
+    else res.header('Cache-Control', 'stale-while-revalidate=7200').json(result);
   }
   catch (err) {
     console.log(err);
