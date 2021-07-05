851f92177ce613b75c072f01e1a64c9bea44e4b8
Dorota Tomala <doforever@gmail.com>
2021-07-05 14:47:39 +0200

    Style product navigation

--- src/components/features/ProductNav/ProductNav.js
+++ src/components/features/ProductNav/ProductNav.js
@@ -6,7 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Link as RouterLink } from 'react-router-dom';
+import { NavLink as RouterLink } from 'react-router-dom';
 
 import styles from './ProductNav.module.scss';
 
@@ -16,7 +16,14 @@ const ProductNav = ({ className, products }) => {
     <Paper component='aside' className={clsx(className, styles.root)}>
       <List>
         { products.map(({id, name}) => (
-          <ListItem key={id} button component={RouterLink} to={`/products/${id}`}>
+          <ListItem
+            key={id}
+            className={styles.item}
+            button
+            component={RouterLink}
+            activeClassName={styles.active}
+            to={`/products/${id}`}
+          >
             <ListItemText primary={name} />
           </ListItem>
         ))}
