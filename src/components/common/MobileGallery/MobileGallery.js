4f45e897ada83828ab9d90d437a42333f1be6783
Dorota Tomala <doforever@gmail.com>
2021-07-16 14:37:56 +0200

    Simplify mobile gallery markup

--- src/components/common/MobileGallery/MobileGallery.js
+++ src/components/common/MobileGallery/MobileGallery.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Grid from '@material-ui/core/Grid';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import SwipeableViews from 'react-swipeable-views';
 import ImageLoader from '../../common/ImageLoader/ImageLoader';
@@ -11,31 +10,28 @@ import styles from './MobileGallery.module.scss';
 const MobileGallery = ({ pictures, current = 0, setCurrent }) => {
 
   return (
-    <Grid container direction='column' className={styles.root}>
-      <Grid item xs className={styles.photo_wrapper}>
-        <SwipeableViews
-          index={current}
-          onChangeIndex={setCurrent}
-          enableMouseEvents
-          slideStyle={{ overflow: 'hidden' }}
-          containerStyle={{ height: '100%' }}
-        >
-          { pictures.map((picture, index) => (
-            <ImageLoader key={index}>
-              <img src={picture.src} alt={picture.name} />
-            </ImageLoader>
-          ))}
-        </SwipeableViews>
-      </Grid>
-      { pictures.length > 0 && <Grid item>
+    <div className={styles.root}>
+      <SwipeableViews
+        index={current}
+        onChangeIndex={setCurrent}
+        enableMouseEvents
+        slideStyle={{ overflow: 'hidden' }}
+        containerStyle={{ height: '100%' }}
+      >
+        { pictures.map((picture, index) => (
+          <ImageLoader key={index}>
+            <img src={picture.src} alt={picture.name} />
+          </ImageLoader>
+        ))}
+      </SwipeableViews>
+      { pictures.length > 0 &&
         <MobileStepper
           variant="dots"
           steps={pictures.length}
           activeStep={current}
           className={styles.dots}
-        />
-      </Grid>}
-    </Grid>
+        />}
+    </div>
   );
 };
 
